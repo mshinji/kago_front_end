682c07f3f908dd8050525cdccf8811a0c1c81bcc
zurukumo <zurukumo@gmail.com>
2021-03-26 03:24:10 +0900

    [add] リーチ成立

--- app/src/components/Constants.tsx
+++ app/src/components/Constants.tsx
@@ -20,8 +20,8 @@ export const Constants = {
   NoticeFieldHeight: 160 /* NoticeHeigth * 2 + 20*/,
   NoticeWidth: 100,
   NoticeHeight: 70,
-  ResultFieldWidth: 500,
-  ResultFieldHeight: 500,
+  KyokuResultFieldWidth: 500,
+  KyokuResultFieldHeight: 500,
   // 牌の大きさ
   OriginalPaiHeight: 45,
   OriginalPaiWidth: 32,
