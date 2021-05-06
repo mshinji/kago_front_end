a440b8a9fa32cfe1c847fe41dea7ff221d5e5c94
zurukumo <zurukumo@gmail.com>
2021-05-07 06:51:38 +0900

    feat: 画像素材追記

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -5,16 +5,21 @@ import styled from 'styled-components';
 export const Footer = () => {
   return (
     <Wrapper>
-      <GitHub>
-        <Link href="https://github.com/mshinji/kago_frontend">
-          GitHub[frontend]
+      <div>
+        <Link href="https://div.com/mshinji/kago_frontend">
+          github[frontend]
         </Link>
-      </GitHub>
-      <GitHub>
-        <Link href="https://github.com/mshinji/kago_backend">
-          GitHub[backend]
-        </Link>
-      </GitHub>
+      </div>
+      <div>
+        <Link href="https://div.com/mshinji/kago_backend">github[backend]</Link>
+      </div>
+      <div>
+        <Link href="https://div.com/mshinji/mahjong">github[mahjong]</Link>
+      </div>
+      <div>
+        画像素材:
+        <Link href="https://majandofu.com/mahjong-images">麻雀豆腐様</Link>
+      </div>
     </Wrapper>
   );
 };
@@ -26,5 +31,3 @@ const Wrapper = styled.div`
   align-items: center;
   color: white;
 `;
-
-const GitHub = styled.div``;
