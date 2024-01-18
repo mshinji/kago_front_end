679d6ec157afc9ad98c6ff292953d356e8a3b276
zurukumo <zurukumo@gmail.com>
2024-01-18 21:10:58 +0900

    feat: vite導入

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Footer = () => {
+  return (
+    <Wrapper>
+      <div>
+        画像素材: <a href="https://majandofu.com/mahjong-images">麻雀豆腐様</a>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+`
---
+++ src/components/Template.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+import { Constants } from './Constants'
+import { Footer } from './Footer'
+import { GameField } from './GameField'
+import { Header } from './Header'
+import { ModeSelect } from './ModeSelect'
+
+const { TemplateWidth } = Constants
+
+export const Template = () => {
+  return (
+    <Wrapper>
+      <Header />
+      <ModeSelect />
+      <GameField />
+      <Footer />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-width: 100%;
+  min-height: 100vh;
+  background: black;
+`
