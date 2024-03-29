b95d6c41e2fc06e2602a613c8e9e03b99faf6b6b
zurukumo <zurukumo@gmail.com>
2024-03-30 01:16:18 +0900

    fix: FooterのGitHubリンクを削除

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,17 +6,6 @@ export const Footer = () => {
   return (
     <Wrapper>
       <div>
-        <Link href="https://div.com/mshinji/kago_frontend">
-          github[frontend]
-        </Link>
-      </div>
-      <div>
-        <Link href="https://div.com/mshinji/kago_backend">github[backend]</Link>
-      </div>
-      <div>
-        <Link href="https://div.com/mshinji/mahjong">github[mahjong]</Link>
-      </div>
-      <div>
         画像素材:
         <Link href="https://majandofu.com/mahjong-images">麻雀豆腐様</Link>
       </div>
