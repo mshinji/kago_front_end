73565634266da347b176197195ffd83f64c54128
matsuda.shinji <thepumpkin404@gmail.com>
2024-03-29 21:44:22 +0900

    Revert "PRを#1から#3までリバート"
    This reverts commit b43ffbdb8458cb953b095becf3b9da9391ae3522, reversing
    changes made to ab34a002ffd3f9521b20e06aa4a6360291e8ffbb.

---
+++ app/src/components/PlayerInfo.tsx
@@ -0,0 +1,104 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { Constants } from './Constants';
+import { Context } from './Context';
+
+interface PlayerInfoProps {
+  who: number;
+}
+
+export const PlayerInfo = (props: PlayerInfoProps) => {
+  const { gameInfo } = useContext(Context);
+  return (
+    <Wrapper who={props.who}>
+      <Row>
+        <Kaze>{gameInfo.kazes[props.who]}</Kaze>
+        <Score>{gameInfo.scores[props.who]}</Score>
+      </Row>
+      {gameInfo.richis[props.who] ? (
+        <Reach>
+          <img src={`/static/images/bou0.png`} />
+        </Reach>
+      ) : (
+        <></>
+      )}
+    </Wrapper>
+  );
+};
+
+const {
+  HyojiWidth,
+  HyojiHeight,
+  PlayerInfoWidth,
+  PlayerInfoHeight,
+  KyokuInfoWidth,
+  KyokuInfoHeight,
+} = Constants;
+
+const calcTranslateX = (who: number) =>
+  [
+    (HyojiWidth - KyokuInfoWidth) / 2,
+    (HyojiWidth + KyokuInfoWidth - (PlayerInfoWidth - PlayerInfoHeight)) / 2,
+    (HyojiWidth - KyokuInfoWidth) / 2,
+    (HyojiWidth - KyokuInfoWidth - (PlayerInfoWidth - PlayerInfoHeight)) / 2 -
+      PlayerInfoHeight,
+  ][who];
+
+const calcTranslateY = (who: number) =>
+  [
+    (HyojiHeight + KyokuInfoHeight) / 2,
+    (HyojiHeight - KyokuInfoHeight + (PlayerInfoWidth - PlayerInfoHeight)) / 2,
+    (HyojiHeight - KyokuInfoHeight) / 2 - PlayerInfoHeight,
+    (HyojiHeight - KyokuInfoHeight + (PlayerInfoWidth - PlayerInfoHeight)) / 2,
+  ][who];
+
+const calcRotateDegree = (who: number) => who * -90;
+
+const Wrapper = styled.div.attrs((props: PlayerInfoProps) => ({
+  who: props.who,
+}))`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  width: ${PlayerInfoWidth}px;
+  height: ${PlayerInfoHeight}px;
+  /* background: gray; */
+  transform: translate(
+      ${(props) => calcTranslateX(props.who)}px,
+      ${(props) => calcTranslateY(props.who)}px
+    )
+    rotate(${(props) => calcRotateDegree(props.who)}deg);
+  transform-origin: center center 0;
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 4px;
+`;
+
+const Kaze = styled.div`
+  height: 20px;
+  line-height: 20px;
+  padding: 2px;
+  margin-right: 5px;
+  background-color: orange;
+  border-radius: 5px;
+`;
+
+const Score = styled.div`
+  height: 24px;
+  line-height: 24px;
+  color: white;
+  font-weight: bold;
+`;
+
+const Reach = styled.div`
+  width: 100%;
+  height: 12px;
+  line-height: 12px;
+  text-align: center;
+`;
